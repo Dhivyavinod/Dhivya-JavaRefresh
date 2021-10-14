f2a57828d2539b0c0074d3ff2b4a1fcc80ed2476
Dhivyavinod <92423423+Dhivyavinod@users.noreply.github.com>
2021-10-14 08:02:27 +0800

    Add files via upload
    Source Files

---
+++ Person.java
@@ -0,0 +1,32 @@
+package com.refresh;
+
+public class Person {
+
+	private String fName;
+    private String lname;
+	/**
+	 * @return the fName
+	 */
+	public String getfName() {
+		return fName;
+	}
+	/**
+	 * @param fName the fName to set
+	 */
+	public void setfName(String firstName) {
+		this.fName = firstName;
+	}
+	/**
+	 * @return the lname
+	 */
+	public String getLname() {
+		return lname;
+	}
+	/**
+	 * @param lname the lname to set
+	 */
+	public void setLname(String lastName) {
+		this.lname = lastName;
+	}
+	
+}
